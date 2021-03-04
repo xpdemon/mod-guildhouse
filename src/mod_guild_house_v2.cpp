0f92d9702e34fb09d20da22c05016d02e9c2e4d7
Vitaliy <mitradis@gmail.com>
2021-03-04 14:31:30 +0300

    fix for last PR ACE remove (#20)

--- src/mod_guild_house_v2.cpp
+++ src/mod_guild_house_v2.cpp
@@ -671,28 +671,6 @@ public:
     }
 };
 
-class GuildHouseConf : public WorldScript
-{
-public:
-    GuildHouseConf() : WorldScript("GuildHouseConf") {}
-
-    void OnBeforeConfigLoad(bool reload) override
-    {
-        if (!reload) {
-            std::string conf_path = _CONF_DIR;
-            std::string cfg_file = conf_path + "/mod_guild_house_v2.conf";
-
-#ifdef WIN32
-            cfg_file = "mod_guild_house_v2.conf";
-#endif
-
-            std::string cfg_def_file = cfg_file + ".dist";
-            sConfigMgr->LoadMore(cfg_def_file.c_str());
-            sConfigMgr->LoadMore(cfg_file.c_str());
-        }
-    }
-};
-
 class GuildHouseGlobal : public GlobalScript
 {
 public:
