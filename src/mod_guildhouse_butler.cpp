6208c6f2b32eb13577e9a44913ba40be0bf81ac4
helldragonpz <46687151+helldragonpz@users.noreply.github.com>
2023-02-26 01:07:37 +0200

    Update mod_guildhouse_butler.cpp (#41)
    GetGOData was changed to GetGameObjectData and the module has been updated

--- src/mod_guildhouse_butler.cpp
+++ src/mod_guildhouse_butler.cpp
@@ -395,7 +395,7 @@ public:
         }
 
         // TODO: is it really necessary to add both the real and DB table guid here ?
-        sObjectMgr->AddGameobjectToGrid(guidLow, sObjectMgr->GetGOData(guidLow));
+        sObjectMgr->AddGameobjectToGrid(guidLow, sObjectMgr->GetGameObjectData(guidLow));
         player->ModifyMoney(-cost);
         CloseGossipMenuFor(player);
     }
