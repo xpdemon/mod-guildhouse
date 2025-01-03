549a9df94268e2904af1506c7f4684bc07ac1740
Seles <boujenah.bruno@gmail.com>
2025-01-03 13:36:20 +0100

    putain de i

--- src/mod_guildhouse.cpp
+++ src/mod_guildhouse.cpp
@@ -409,7 +409,7 @@ public:
     static void CheckPlayer(Player *player) {
         auto *guildData = player->CustomData.GetDefault<GuildData>("phase");
         QueryResult result = CharacterDatabase.Query(
-            "SELECT `id`, `guild`, `phase`, `map`,`positionX`, `positionY`, `positionZ`, `orientation`,`ìnstanceId`,`firstVisit` FROM guild_house WHERE `guild` = {}",
+            "SELECT `id`, `guild`, `phase`, `map`,`positionX`, `positionY`, `positionZ`, `orientation`,`instanceId`,`firstVisit` FROM guild_house WHERE `guild` = {}",
             player->GetGuildId());
 
         if (result) {
