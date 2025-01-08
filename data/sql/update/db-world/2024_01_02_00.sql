1f0c5c093050bb0cbc0a295c7e864016e3b877b5
Seles <boujenah.bruno@gmail.com>
2025-01-08 11:20:37 +0100

    manager

--- data/sql/update/db-world/2024_01_02_00.sql
+++ data/sql/update/db-world/2024_01_02_00.sql
@@ -1,2 +1,8 @@
-Alter Table `guild_house_spawns`
-    Add Column `map` Int Unsigned Not Null Default 0 After `comment`;
+DELETE FROM `creature_template` WHERE (`entry` = 500033);
+INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
+    (500033, 0, 0, 0, 0, 0, 'Xrispins', 'Guild House Butler', '', 0, 35, 35, 0, 35, 0, 1, 1.14286, 1, 1, 20, 1, 0, 0, 1, 2000, 2000, 1, 1, 1, 33536, 2048, 0, 0, 0, 0, 0, 0, 7, 4096, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 'GuildMjHouseSpawner', 12340);
+
+DELETE FROM `creature_template_model` WHERE (`CreatureID` = 500033);
+INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
+    (500033, 0, 13355, 1, 1, 0);
+
